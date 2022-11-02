dd2bd6cffbb3a445ec1390b67f33a4313a545054
Kasthuri3695 <115356473+Kasthuri3695@users.noreply.github.com>
2022-11-02 22:41:30 +0530

    Add files via upload

---
+++ Add_Two_Numbers.java
@@ -0,0 +1,12 @@
+class AddtwoNumbers
+{
+public static void main(String[] args)
+{
+int a=5, b=10, c, d; 
+c=a+b;
+d=a*b;
+System.out.println("Sum of these numbers" + c);
+System.out.println("Multiply of these numbers" + d);
+}
+}
+
