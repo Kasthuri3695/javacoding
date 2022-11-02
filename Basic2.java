8c1700397cd1bab384dbf9763fbfc556a3f052b5
Kasthuri3695 <115356473+Kasthuri3695@users.noreply.github.com>
2022-11-02 22:42:22 +0530

    Add files via upload

---
+++ Basic2.java
@@ -0,0 +1,18 @@
+class Basic
+{
+public static void main(String[] args)
+{
+Basic basic = new Basic();
+basic.add();
+basic.subtract();
+}
+void add()
+{
+System.out.println(10+20);
+}
+
+void subtract()
+{
+System.out.println(20-10);
+}
+}
